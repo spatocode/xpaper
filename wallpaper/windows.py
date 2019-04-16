711c6b32a4cdfba778f85ce182a64bc537773795
Spatocode <impulseofficial069@gmail.com>
2019-04-16 19:57:00 +0100

    set windows background from file

--- wallpaper/windows.py
+++ wallpaper/windows.py
@@ -1,4 +1,11 @@
+import sys
 import ctypes
 
 SPI_SETDESKWALLPAPER = 0x0014
 SPI_GETDESKWALLPAPER = 0x0073
+
+def setfromfile(imagepath):
+    if sys.version_info[0] is 2:
+        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imagepath, 3)
+    else:
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, imagepath, 3)

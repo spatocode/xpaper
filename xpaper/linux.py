456e9e1f0853365d911ed973fc99fda255b03415
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-04-28 22:27:51 +0100

    refactor/fix bug in getting wallpaper on linux

--- xpaper/linux.py
+++ xpaper/linux.py
@@ -11,41 +11,53 @@ def setwallpaper(imagepath):
     desktop_session = os.environ.get("DESKTOP_SESSION")
     if desktop_session is not None:
         desktop_session = desktop_session.lower()
-        if desktop_session in ["gnome", "unity", "cinnamon", "pantheon"]:
-            subprocess.Popen("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "'file://%s'" % imagepath)
+        command = None
+        if desktop_session in ["gnome", "ubuntu", "unity", "cinnamon", "pantheon"]:
+            command = ["gsettings", "set", "org.gnome.desktop.background", "picture-uri", "'file://%s'" % imagepath]
         elif desktop_session == "mate":
             try:
-                subprocess.Popen("dconf", "write", "/org/mate/desktop/background/picture-filename", imagepath)
+                command = ["dconf", "write", "/org/mate/desktop/background/picture-filename", imagepath]
             except:
-                subprocess.Popen("gsettings", "set", "org.mate.background", "picture-filename", "'%s'" % imagepath)
+                command = ["gsettings", "set", "org.mate.background", "picture-filename", "'%s'" % imagepath]
             finally:
-                 subprocess.Popen("mateconftool-2","-t","string","--set","/desktop/mate/background/picture_filename",'"%s"' % imagepath)
+                command = ["mateconftool-2","-t","string","--set","/desktop/mate/background/picture_filename",'"%s"' % imagepath]
         elif desktop_session == "lxde":
             try:
-                subprocess.Popen("pcmanfm", "-w", imagepath)
+                command = ["pcmanfm", "-w", imagepath]
             except:
-                subprocess.Popen("pcmanfm --set-wallpaper %s --wallpaper-mode=scaled" % imagepath)
+                command = ["pcmanfm --set-wallpaper %s --wallpaper-mode=scaled" % imagepath]
         elif desktop_session == "xfce" or desktop_session.startswith("xubuntu"):
-            subprocess.Popen("xfconf-query", "-c", "xfce4-desktop", "-p", "/backdrop/screen0/monitor0/workspace0/last-image", "-s", imagepath)
+            command = ["xfconf-query", "-c", "xfce4-desktop", "-p", "/backdrop/screen0/monitor0/workspace0/last-image", "-s", imagepath]
         elif desktop_session in ["kde", "kde3", "trinity"]:
-            subprocess.Popen("dcop kdesktop KBackgroundIface setWallpaper 0 '%s' 6" % imagepath)
+            command = ["dcop kdesktop KBackgroundIface setWallpaper 0 '%s' 6" % imagepath]
         elif desktop_session in ["fluxbox", "jwm", "openbox", "afterstep"]:
             try:
-                subprocess.Popen("fbsetbg", imagepath)
+                command = ["fbsetbg", imagepath]
             except:
                 sys.stderr.write("ERROR: Failed to set wallpaper with fbsetbg!\n")
                 sys.stderr.write("Please make sure you have fbsetbg installed.\n")
         elif desktop_session == "blackbox":
-            subprocess.Popen("bsetbg", "-full", imagepath)
+            command = ["bsetbg", "-full", imagepath]
         elif desktop_session == "windowmaker":
-            subprocess.Popen("wmsetbg -s -u %s" % imagepath, shell=True)
+            command = ["wmsetbg -s -u %s" % imagepath]
+        exec_command(command)
 
+def exec_command(command):
+    if not command:
+        return
+    output = subprocess.check_output(command)
+    path = output.decode("utf-8").strip()[1:-1]
+    return path
 
 def getwallpaper():
     desktop_session = os.environ.get("DESKTOP_SESSION")
     if desktop_session is not None:
         desktop_session = desktop_session.lower()
+        command = None
+        if desktop_session in ["gnome", "ubuntu", "unity", "cinnamon", "pantheon"]:
+            command = ["gsettings", "get", "org.gnome.desktop.background", "picture-uri"]
         if desktop_session == "XFCE":
-            return subprocess.Popen("xfconf-query", "-c", "xfce4-desktop", "-p", "/backdrop/screen0/monitor0/workspace0/last-image")
+            command = ["xfconf-query", "-c", "xfce4-desktop", "-p", "/backdrop/screen0/monitor0/workspace0/last-image"]
         elif desktop_session == "MATE":
-            return subprocess.Popen("dconf", "read", "/org/mate/desktop/background/picture-filename")
+            command = ["dconf", "read", "/org/mate/desktop/background/picture-filename"]
+        return exec_command(command)
